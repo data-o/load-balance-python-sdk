f7828aeb7ecbeca8359f8feced40f4a647dcb07b
gerrit <gerrit@baidu.com>
2019-09-02 10:20:18 +0800

    init

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+################################################################################
+#
+# Copyright (c) 2019 Baidu.com, Inc. All Rights Reserved
+#
+################################################################################
+"""
+Setup script.
+
+Authors: liupeng37(liupeng37@baidu.com)
+Date:    2019/09/02 10:20:18
+"""
+
+import setuptools
+
+setuptools.setup()
+
